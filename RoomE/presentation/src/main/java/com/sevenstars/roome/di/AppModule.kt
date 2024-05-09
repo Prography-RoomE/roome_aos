68df007155bade19deaecfc69c2b9f3a189940e8
moondev03 <moondev03@gmail.com>
2024-05-09 11:48:58 +0900

    :sparkles: (feat) AppModule | Retrofit 추가

---
+++ RoomE/presentation/src/main/java/com/sevenstars/roome/di/AppModule.kt
@@ -0,0 +1,38 @@
+package com.sevenstars.roome.di
+
+import com.google.gson.GsonBuilder
+import com.sevenstars.roome.BuildConfig
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+            .setLevel(HttpLoggingInterceptor.Level.BODY)
+        return OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun providesRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().setLenient().create()))
+            .client(okHttpClient)
+            .baseUrl(BuildConfig.BASE_URL)
+            .build()
+    }
+}
