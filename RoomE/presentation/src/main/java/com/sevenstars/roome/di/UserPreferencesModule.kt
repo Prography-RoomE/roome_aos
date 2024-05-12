fc8f3499c34d5bb92cd17363b4fc4f70121dfc69
moondev03 <moondev03@gmail.com>
2024-05-12 17:26:06 +0900

    (feat) datastore | UserPreferencesModule

---
+++ RoomE/presentation/src/main/java/com/sevenstars/roome/di/UserPreferencesModule.kt
@@ -0,0 +1,41 @@
+package com.sevenstars.roome.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import com.sevenstars.data.repository.auth.UserPreferencesRepositoryImpl
+import com.sevenstars.domain.repository.auth.UserPreferencesRepository
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+val Context.userDataStore: DataStore<Preferences> by preferencesDataStore(
+    name = "com.sevenstars.roome.user_preferences"
+)
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class UserPreferencesModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindUserPreferencesRepository(
+        userPreferencesRepositoryImpl: UserPreferencesRepositoryImpl
+    ): UserPreferencesRepository
+
+    companion object {
+
+        @Provides
+        @Singleton
+        fun provideUserDataStorePreferences(
+            @ApplicationContext applicationContext: Context
+        ): DataStore<Preferences> {
+            return applicationContext.userDataStore
+        }
+    }
+}
