d57289ec00d3e3ba586e724457bc60423e1e9c12
moondev03 <moondev03@gmail.com>
2024-05-12 17:27:41 +0900

    (cleanup) Module | UseCase Module Feature 별로 분리

---
+++ RoomE/presentation/src/main/java/com/sevenstars/roome/di/UserUseCaseModule.kt
@@ -0,0 +1,22 @@
+package com.sevenstars.roome.di
+
+import com.sevenstars.domain.repository.UserRepository
+import com.sevenstars.domain.usecase.user.GetUserInfoUseCase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object UserUseCaseModule {
+
+    @Provides
+    @Singleton
+    fun provideGetUserInfoUseCase(
+        repository: UserRepository
+    ): GetUserInfoUseCase {
+        return GetUserInfoUseCase(repository = repository)
+    }
+}
