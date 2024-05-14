ad6fee3df3e5f893b74fc5b1c343409f19d0074f
moondev03 <moondev03@gmail.com>
2024-05-15 04:01:43 +0900

    (feat) 회원가입 | 약관 동의 설정 및 외부 링크 연결

--- RoomE/presentation/src/main/java/com/sevenstars/roome/view/signup/SignUpActivity.kt
+++ RoomE/presentation/src/main/java/com/sevenstars/roome/view/signup/SignUpActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.viewModels
 import com.sevenstars.roome.R
 import com.sevenstars.roome.base.BaseActivity
 import com.sevenstars.roome.databinding.ActivitySignUpBinding
+import com.sevenstars.roome.view.MainActivity
 import com.sevenstars.roome.view.signIn.SignInActivity
 
 class SignUpActivity: BaseActivity<ActivitySignUpBinding>(R.layout.activity_sign_up) {
@@ -20,6 +21,12 @@ class SignUpActivity: BaseActivity<ActivitySignUpBinding>(R.layout.activity_sign
         val intent = Intent(this, SignInActivity::class.java)
         // 호출하는 Activity가 스택에 있을 경우, 해당 Activity를 최상위로 올리면서, 그 위에 있던 Activity들을 모두 삭제
         intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+        startActivity(intent)
+        finish()
+    }
+
+    fun moveToMain(){
+        val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
         finish()
     }
--- RoomE/presentation/src/main/java/com/sevenstars/roome/view/signup/SignupAgreeFragment.kt
+++ RoomE/presentation/src/main/java/com/sevenstars/roome/view/signup/SignupAgreeFragment.kt
@@ -1,9 +1,15 @@
 package com.sevenstars.roome.view.signup
 
+import android.content.Intent
+import android.net.Uri
+import android.widget.CompoundButton
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.activityViewModels
+import com.sevenstars.data.utils.LoggerUtils
 import com.sevenstars.roome.R
 import com.sevenstars.roome.base.BaseFragment
 import com.sevenstars.roome.databinding.FragmentSignupAgreeBinding
+import com.sevenstars.roome.utils.UiState
 
 class SignupAgreeFragment: BaseFragment<FragmentSignupAgreeBinding>(R.layout.fragment_signup_agree) {
     private val viewModel: SignUpViewModel by activityViewModels()
@@ -18,10 +24,67 @@ class SignupAgreeFragment: BaseFragment<FragmentSignupAgreeBinding>(R.layout.fra
         binding.btnBack.setOnClickListener {
             requireActivity().supportFragmentManager.popBackStack()
         }
+
+        binding.btnNext.setOnClickListener {
+            if(binding.cbAgreeAge.isChecked && binding.cbAgreeService.isChecked && binding.cbAgreePrivacy.isChecked) (requireActivity() as SignUpActivity).moveToMain()
+        }
+
+        binding.apply {
+            ibOpenService.setOnClickListener{openBrowser("Service")}
+            ibOpenPrivacy.setOnClickListener{openBrowser("Privacy")}
+            ibOpenAd.setOnClickListener{openBrowser("Ad")}
+        }
+
+        val checkedChangeListener = CompoundButton.OnCheckedChangeListener { _, _ ->
+            with(binding){
+                if(cbAgreeAge.isChecked && cbAgreeService.isChecked && cbAgreePrivacy.isChecked){
+                    btnNext.apply {
+                        backgroundTintList = ContextCompat.getColorStateList(requireContext(), R.color.primary_primary)
+                        setTextColor(ContextCompat.getColor(requireContext(), R.color.surface))
+                    }
+                } else {
+                    btnNext.apply {
+                        backgroundTintList = ContextCompat.getColorStateList(requireContext(), R.color.on_surface_a12)
+                        setTextColor(ContextCompat.getColor(requireContext(), R.color.on_surface_variant))
+                    }
+                }
+            }
+        }
+
+        binding.apply {
+            cbAgreeAge.setOnCheckedChangeListener(checkedChangeListener)
+            cbAgreeService.setOnCheckedChangeListener(checkedChangeListener)
+            cbAgreePrivacy.setOnCheckedChangeListener(checkedChangeListener)
+            cbAgreeAd.setOnCheckedChangeListener(checkedChangeListener)
+        }
     }
 
     override fun observer() {
         super.observer()
 
+        viewModel.uiState.observe(this){
+            when(it){
+                is UiState.Failure -> {
+                    showToast(it.message)
+                    LoggerUtils.error(it.message)
+                }
+                is UiState.Loading -> {}
+                is UiState.Success -> {
+
+                }
+            }
+        }
+    }
+
+    private fun openBrowser(target: String){
+        val webpage = when(target){
+            "Service" -> "https://google.com"
+            "Privacy" -> "https://google.com"
+            "Ad" -> "https://google.com"
+            else -> "https://google.com"
+        }
+
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(webpage))
+        if(intent.resolveActivity(requireActivity().packageManager) != null) startActivity(intent)
     }
 }
