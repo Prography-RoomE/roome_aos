6da91e458752561ad7e9379a53791bcb045fb6ab
moondev03 <moondev03@gmail.com>
2024-05-17 12:42:17 +0900

    (feat) 회원가입 | UseCase Module Provides 추가, ViewModel Inject

--- RoomE/presentation/src/main/java/com/sevenstars/roome/view/signup/SignUpViewModel.kt
+++ RoomE/presentation/src/main/java/com/sevenstars/roome/view/signup/SignUpViewModel.kt
@@ -3,9 +3,21 @@ package com.sevenstars.roome.view.signup
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sevenstars.domain.usecase.user.SaveNickUseCase
+import com.sevenstars.domain.usecase.user.SaveTermsAgreementUseCase
+import com.sevenstars.domain.usecase.user.ValidationNickUseCase
 import com.sevenstars.roome.utils.UiState
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class SignUpViewModel: ViewModel() {
+class SignUpViewModel @Inject constructor(
+    private val validationNickUseCase: ValidationNickUseCase,
+    private val saveTermsAgreementUseCase: SaveTermsAgreementUseCase,
+    private val saveNickUseCase: SaveNickUseCase
+): ViewModel() {
+
+
     // 회원가입 요청 상태
     private val _uiState = MutableLiveData<UiState<Unit>>(UiState.Loading)
     val uiState: LiveData<UiState<Unit>> get() = _uiState
@@ -19,6 +31,11 @@ class SignUpViewModel: ViewModel() {
     }
 
     fun checkNick(){
+        viewModelScope.launch {
+
+        }
+
+
         _checkState.value = UiState.Success(Unit)
     }
 
