74b0c0aaeaf8869fe2411ec0a148588e9c1fc31b
MunJangHun <105299421+moondev03@users.noreply.github.com>
2024-06-06 16:54:04 +0900

    1차 QA 반영 (#34)
    * (fix) QA | 프로필 생성 이어하기 다이얼로그 라이팅 및 버튼 색 수정
    
    * (fix) QA | 프로필 생성 중 방탈출 경험 횟수 Soft Keyboard 처리
    
    * (fix) QA | 프로필 생성 중 사용된 버튼들 그림자 제거
    
    * (fix) QA | 프로필 1:1 정방형 라이팅 추가
    
    * (fix) QA | 프로필 3:4 세로형 이미지에서 라이팅 수정

--- RoomE/presentation/src/main/java/com/sevenstars/roome/custom/CustomToast.kt
+++ RoomE/presentation/src/main/java/com/sevenstars/roome/custom/CustomToast.kt
@@ -2,16 +2,12 @@ package com.sevenstars.roome.custom
 
 import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
-import android.widget.ImageView
-import android.widget.TextView
 import android.widget.Toast
 import com.sevenstars.roome.databinding.ToastCustomBinding
 
 
 object CustomToast {
     fun makeToast(context: Context?, msg: String): Toast {
-        val inflater = LayoutInflater.from(context)
         val binding = ToastCustomBinding.inflate(LayoutInflater.from(context))
 
         binding.tvMsg.text = msg
