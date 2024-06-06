83d9949d4094fb8fbea7b420e860cfd4e53d5fa4
moondev03 <moondev03@gmail.com>
2024-06-06 19:24:47 +0900

    (feat) 회원탈퇴

--- RoomE/presentation/src/main/java/com/sevenstars/roome/view/MainActivity.kt
+++ RoomE/presentation/src/main/java/com/sevenstars/roome/view/MainActivity.kt
@@ -3,8 +3,12 @@ package com.sevenstars.roome.view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.sevenstars.data.utils.LoggerUtils
+import com.sevenstars.domain.repository.auth.AuthRepository
 import com.sevenstars.domain.repository.auth.UserPreferencesRepository
+import com.sevenstars.roome.base.RoomeApplication.Companion.app
+import com.sevenstars.roome.custom.CustomToast
 import com.sevenstars.roome.databinding.ActivityMainBinding
 import com.sevenstars.roome.view.splash.StartActivity
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,6 +24,9 @@ class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var userPreferencesRepository: UserPreferencesRepository
 
+    @Inject
+    lateinit var authRepository: AuthRepository
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -27,14 +34,24 @@ class MainActivity : AppCompatActivity() {
 
         binding.btnLogout.setOnClickListener {
             CoroutineScope(Dispatchers.IO).launch {
-                userPreferencesRepository.clearData()
+                authRepository.unlink(app.userPreferences.getAccessToken().getOrNull().orEmpty(), code=null, provider = app.userPreferences.getLoginProvider().getOrNull().orEmpty())
                     .onSuccess {
-                        val intent = Intent(this@MainActivity, StartActivity::class.java)
-                        startActivity(intent)
-                        finish()
-                    }.onFailure {
-                        LoggerUtils.error(it.stackTraceToString())
+                        launch {
+                            userPreferencesRepository.clearData()
+                                .onSuccess {
+                                    val intent = Intent(this@MainActivity, StartActivity::class.java)
+                                    startActivity(intent)
+                                    finish()
+                                }.onFailure {
+                                    LoggerUtils.error(it.stackTraceToString())
+                                }
+                        }
+                    }.onFailure { _, msg ->
+                        LoggerUtils.error(msg)
+                        CustomToast.makeToast(this@MainActivity, "회원탈퇴 실패").show()
                     }
+
+
             }
         }
     }
